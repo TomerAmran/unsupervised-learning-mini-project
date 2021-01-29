7bbd4d169ad8011087d6405a8057bbb1195c490a
tomeramran <tomera@bigvu.tv>
2021-01-29 12:50:29 +0200

    seperate multiplier for depth (implemented in the new function

--- rgbd_test/rgbd_segmentation.jl
+++ rgbd_test/rgbd_segmentation.jl
@@ -115,12 +115,61 @@ function segment_rgbd(img_path,depth_path,out_path,rgb_prior_multiplier,xyz_prio
     save(out_path, segemnated_image)
 end
 
+function segment_rgbd_with_seperate_depth_weight(img_path,depth_path,out_path,rgb_prior_multiplier,xy_prior_multiplier,depth_multiplier,nu)
+    img = load(img_path)
+    depth = load(depth_path)
+    #depth normalization
+    depth = Float32.(depth)
+    dt = StatsBase.fit(UnitRangeTransform, depth)
+    depth = StatsBase.transform(dt, depth)
+    # depth = Kinect_DepthNormalization(depth)
+    #Change to channel view
+    img_channels = channelview(img)
+    rgb,x,y = size(img_channels)
+    #Create input
+    input_arr = zeros(6,x*y)
+    for i=1:x
+        for j=1:y
+            input_arr[:,(i-1)*y+j]=[img_channels[1,i,j],img_channels[2,i,j],img_channels[3,i,j],i,j,depth[i,j]]
+        end
+    end
+    #Create HyperParams
+    #The rgb,xy multiplier allows us to both play with the weight of the xy/rgb
+    # rgb_prior_multiplier = 30 
+    # xyz_prior_multiplier = 1
+    data_cov = cov(input_arr')
+    data_cov[4:6,1:3] .= 0
+    data_cov[1:3,4:6] .= 0
+    # data_cov[1:5,6] .= 0 
+    # data_cov[6,1:5] .= 0
+    data_cov[1:3,1:3] .*= rgb_prior_multiplier
+    data_cov[4:5,4:5] .*= xy_prior_multiplier
+    data_cov[6,6] *= depth_multiplier
+    data_mean = mean(input_arr,dims = 2)[:]
+    hyper_params = DPMMSubClusters.niw_hyperparams(0.5,
+            data_mean,
+            nu, #nu
+            data_cov)
+    #Run the model
+    labels,clusters,weights = DPMMSubClusters.fit(input_arr,hyper_params,50000.0,iters = 200, verbose = true)
+    #Get the cluster color means
+    color_means = [x.μ[1:3] for x in clusters]
+    segemnated_image = zeros(3,x,y)
+    for i=1:x
+        for j=1:y
+            segemnated_image[:,i,j] = color_means[labels[(i-1)*y+j]]
+        end
+    end
+    segemnated_image = colorview(RGB,segemnated_image)
+    save(out_path, segemnated_image)
+end
 img_path = "images/frame-000010.color.png"
 depth_path = "images/frame-000010.depth.png"
 rgb_prior_multiplier = 30
 xy_prior_multiplier = 1 
-nu = 64
+depth_prior_multiplier = 10
+nu = 8
 println("running..")
 segment_rgb(img_path,"rgb_result.png",rgb_prior_multiplier,xy_prior_multiplier, nu)
-segment_rgbd(img_path,depth_path,"rgbd_result.png",rgb_prior_multiplier,xy_prior_multiplier, nu)
-
+# segment_rgbd(img_path,depth_path,"rgbd_result.png",rgb_prior_multiplier,xy_prior_multiplier,nu)
+segment_rgbd_with_seperate_depth_weight(img_path,depth_path,"rgbd_result.png",rgb_prior_multiplier,xy_prior_multiplier,depth_prior_multiplier,nu)
